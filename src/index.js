ec9b7373847884c1a3c1e934853bf2c82258098d
Jacky Su <stzuchieh@gmail.com>
2023-01-09 22:07:30 +0800

    Third stage first week assignment first version

--- src/index.js
+++ src/index.js
@@ -5,11 +5,6 @@ import { BrowserRouter } from "react-router-dom";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDctcHCN05OHr60Imzg9av7SoNGAhctMBk",
   authDomain: "to-do-list-ad778.firebaseapp.com",
